20fe9e96dcfcd10049087099dec93af8dc9222f1
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-11-17 09:29:52 -0500

    Test a what

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,17 +7,19 @@ import flixel.input.FlxInput;
 class PlayState extends FlxState
 {
 	var last:FlxInputState = W;
+	var press:Bool = false;
 	override public function create()
 	{
 		super.create();
 		add(new FlxText("Welcome to searchtin! The go to search engine for surfing the world wide web! Please select one of the following options to browse:", 32).screenCenter());
 		add(new FlxText("Arg", 32, -14).screenCenter());
 		add(new FlxText("Cow", 32, -14, -14).screenCenter());
+		if (FlxText = "Cow") { pressie(); }
 	}
 	
-	public function press():Void
+	public function pressie():Void
 	{
-	    if(last:FlxInputState = C) {
+	    if(press = true) {
 			remove(FlxText);
 			add(New FlxText("Now displaying results for cow. Happy Meat Farms - The meat that will make you happy", 32).screenCenter());
 	    }
