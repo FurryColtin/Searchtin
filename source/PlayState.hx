238452674ba18f279e8554a4435c44d431e085a6
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-11-17 09:47:50 -0500

    WHY

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,16 +7,13 @@ import flixel.input.FlxInput;
 
 class PlayState extends FlxState
 {
-	var last:FlxInputState = W;
 	var press:Bool = false;
         var pressarg:Bool = false;
 	override public function create()
 	{
 		super.create();
 		add(new FlxText("Welcome to searchtin! The go to search engine for surfing the world wide web! Please select one of the following options to browse:", 32).screenCenter());
-		add(new FlxText("Arg", 32, -14).screenCenter());
-		add(new FlxText("Cow", 32, -14, -14).screenCenter());
-		if (FlxText = "Cow") { pressie(); }
+		if (FlxText = "Welcome to searchtin! The go to search engine for surfing the world wide web! Please select one of the following options to browse:") { pressie(); }
 	}
 	
 	public static function browserLoad(site:String) {
@@ -25,9 +22,10 @@ class PlayState extends FlxState
 	
 	public function pressie():Void
 	{
+	    press = true;
 	    if(press = true) {
-			remove(FlxText);
-		        add(new FlxText("Taking you to the very first search result retrieved.", 32).screenCenter());
+			//remove(FlxText);
+		        //add(new FlxText("Taking you to the very first search result retrieved.", 32).screenCenter());
 		       // site = "https://www.happymeatfarms.com"
 			browserLoad("https://www.happymeatfarms.com");
 			//add(New FlxText("Now displaying results for cow. Happy Meat Farms - The meat that will make you happy", 32).screenCenter());
